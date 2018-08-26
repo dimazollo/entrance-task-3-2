6a070b3b3316af42b4fe44a1d846d0dce13f716e
Dmitry Volovod <dvolovod@it.ru>
2018-08-27 01:26:39 +0300

    add app.js with read and write functions

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const Schedule = require('./resultData')
+
+function readData (inputFilePath) {
+  const fileContent = fs.readFileSync(inputFilePath, 'utf-8');
+  return JSON.parse(fileContent)
+}
+
+function writeData (outputFilePath, dataObject) {
+  const dataJson = JSON.stringify(dataObject)
+  fs.writeFileSync(outputFilePath, dataJson)
+}
+
+
+(
+  function main() {
+    const data = readData('./data/input.json')
+    const { devices, rates, maxPower } = data
+
+    const resultData = {
+      schedule: {},
+      consumedEnergy: {
+        value: 0,
+        devices: {}
+      }
+    }
+
+    const schedule = new Schedule(rates)
+
+    // writeData('./data/output.json', resultData)
+  }
+)()
+
+
+// There are only two modes: 'day' and 'night'
+function modeOfHour (value) {
+  if (value >= 7 && value < 21) {
+    return 'day'
+  } else {
+    return 'night'
+  }
+}
+
+function getFreeMinimalRate (schedule, maxPower, mode) {
+
+  schedule.forEach(element => {
+
+  })
+}
